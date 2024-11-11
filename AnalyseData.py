9707c4b29127ed54ca5dbc198c2acd633b86a846
SophiaCKJ <sck39@cantab.ac.uk>
2024-11-11 18:23:25 +0000

    Imported data and saved the data as csv

--- AnalyseData.py
+++ AnalyseData.py
@@ -1,1 +1,7 @@
 print('Welcome to Analyse Data!')
+import pandas as pd
+# the path to my excel file
+file_path = 'data_glotip.csv'
+# function to find (because i told it to) and read the data in the excel spreadsheet
+data = pd.read_csv(file_path)
+print(data.head())
